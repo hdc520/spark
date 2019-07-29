ef554a0e12194fb622dd84872c018de92e4cbec9
hdc <1712861889@qq.com>
2019-07-29 15:32:30 +0800

    if和else的判断语句用法

---
+++ src/main/scala/scala_learn/If_else.scala
@@ -0,0 +1,17 @@
+package scala_learn
+
+object If_else {
+  def main(args: Array[String]): Unit = {
+    val age=28
+    if (age>28)
+      print(age)
+    else
+      println("小于")
+    var flag=if(age<29) 1 else "hdc"  //运用if和else来赋值
+    println("flag:"+flag)
+    //scala的块表达式,scala的语句终结符是分号,在块表达式中,最后一个语句的值就是块表达式的返回值.
+    var str=if (age>18){var b=age+1;b+2} else "未成年"
+    println("str："+str)
+  }
+
+}
