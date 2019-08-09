beae0b215efed481e94deb2397fac10b6b4689da
hdc <1712861889@qq.com>
2019-08-09 16:34:49 +0800

    自定义的MyIterator迭代器

---
+++ src/main/java/java_learn/iteration/MyIterator.java
@@ -0,0 +1,30 @@
+package java_learn.iteration;
+
+import java.util.Iterator;
+
+public class MyIterator implements Iterator<String> {
+    private String []elem={"a","b","c","d","e"};
+    private int size=elem.length;
+    private int curror=-1;
+
+    public boolean hasNext() {
+        return curror+1<size;
+    }
+
+    public String next() {
+        curror++;
+        return elem[curror];
+    }
+    public void remove() {
+    }
+
+    public static void main(String[] args) {
+        MyIterator myIterator=new MyIterator();
+        while(myIterator.hasNext()){
+            System.out.println(myIterator.next());
+        }
+//        for(String str:MyIterator){
+//            报错是因为实现foreach循环必须要实现Iterable接口
+//        }
+    }
+}
