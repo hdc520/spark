b17119e3c3be32b25c75ff9d18ec97eaa76b7460
hdc <1712861889@qq.com>
2019-08-24 11:26:48 +0800

    累加器

--- src/main/java/java_spark/spark_book/第6章/accumulator.java
+++ src/main/java/java_spark/spark_book/第6章/accumulator.java
@@ -8,7 +8,8 @@ import org.apache.spark.api.java.function.VoidFunction;
 
 import java.util.Arrays;
 import java.util.List;
-
+//累加器用来对信息进行聚合即将工作中的节点的值聚合到驱动器程序中的简单语法
+//工作节点中的任务不能访问累加器中的值，对于工作节点中的任务，累加器是一个只写变量，只能由驱动程序才能访问
 public class accumulator {
     public static void main(String[] args) {
         SparkConf conf=new SparkConf().setAppName("broadcast_var").setMaster("local");
