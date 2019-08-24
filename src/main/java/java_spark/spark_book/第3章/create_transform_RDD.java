638df6a727c8d0a6ca60440f89ed88a18e757288
hdc <1712861889@qq.com>
2019-08-24 10:12:02 +0800

    lambda表达式写RDD的转化

--- src/main/java/java_spark/spark_book/第3章/create_transform_RDD.java
+++ src/main/java/java_spark/spark_book/第3章/create_transform_RDD.java
@@ -9,11 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class create_transform_RDD {
-    public static void main(String args[]){
+    public static void main(String []args){
         SparkConf conf=new SparkConf().setAppName("create_transform_RDD");
         conf.setMaster("local");
         JavaSparkContext sc=new JavaSparkContext(conf);
-        List<String> list= new ArrayList<String>();
+        List<String> list= new ArrayList<>();
         for(int i=0;i<10;i++){
             list.add("num:"+i);
         }
@@ -31,11 +31,13 @@ public class create_transform_RDD {
 
         //对RDD的转化操作即将旧的RDD转化成新的RDD（转化操作）
         // 转化出来的RDD是惰性的求值的，只有在行动操作中用到这些RDD才会被计算。
-        JavaRDD hdcRDD=textRDD.filter(new Function<String, Boolean>() {
-            public Boolean call(String s) throws Exception {
-                return s.contains("hdc");
-            }
-        });
+        JavaRDD hdcRDD=textRDD.filter((Function<String, Boolean>) s -> s.contains("hdc"));
+        //或者
+//        JavaRDD hdcRDD=textRDD.filter(new Function<String, Boolean>() {
+//            public Boolean call(String s) throws Exception {
+//                return s.contains("hdc");
+//            }
+//        });
 
         System.out.println("hdcRDD："+hdcRDD);
     }
