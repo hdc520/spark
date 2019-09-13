682fcda1c9984e74731d56388648655b9c55b561
hdc <1712861889@qq.com>
2019-09-13 21:34:53 +0800

    归并排序

---
+++ src/main/java/java_exam/一般/MergeSort.java
@@ -0,0 +1,43 @@
+package java_exam.一般;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int a[]={2,6,1,3,7,4,5,8,0,9};
+        sort(a,0,a.length-1);
+        for(int i:a){
+            System.out.print(i+" ");
+        }
+    }
+    public static void sort(int a[],int left,int right){
+        if(left==right)
+            return;
+        int mid=(left+right)/2;
+        sort(a,left,mid);
+        sort(a,mid+1,right);
+        merge(a,left,right);
+    }
+    public static void merge(int a[],int left,int right){
+        int mid=(left+right)/2;
+        int i=left;
+        int j=mid+1;
+        int count=0;
+        int []tmp=new int[right-left+1];
+        while (i<=mid&&j<=right){
+            if(a[i]<a[j]){
+                tmp[count++]=a[i++];
+            }
+            else {
+                tmp[count++]=a[j++];
+            }
+        }
+        while (i<=mid){
+            tmp[count++]=a[i++];
+        }
+        while (j<=right){
+            tmp[count++]=a[j++];
+        }
+        for(i=0;i<tmp.length;i++){
+            a[left+i]=tmp[i];
+        }
+    }
+}
