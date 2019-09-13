17f4cd906e68cf6215d33bbca7f42d9fb92ecaba
hdc <1712861889@qq.com>
2019-09-13 20:16:08 +0800

    双向冒泡排序

--- src/main/java/java_exam/一般/Bsort.java
+++ src/main/java/java_exam/一般/Bsort.java
@@ -2,7 +2,7 @@ package java_exam.一般;
 
 public class Bsort {
     public static void main(String[] args) {
-        int a[]={1,2,4,4,4,9,4,6,0},flag=0;
+        int a[]={8,7,6,12,5,4,3,2,1,0},flag=0;
         for(int i=0;i<a.length;i++){
             flag=0;
             for(int j=0;j<a.length-1-i;j++){
@@ -17,6 +17,32 @@ public class Bsort {
                 break;
         }
         for(int i:a){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        int b[]={8,7,6,12,5,4,3,2,1,0};
+        int left=0,shift=1,right=b.length-1;
+        while(left<right){
+            for(int i=left;i<right;i++){
+                if(b[i]>b[i+1]){
+                    int tmp=b[i];
+                    b[i]=b[i+1];
+                    b[i+1]=tmp;
+                    shift=i;
+                }
+            }
+            right=shift;
+            for(int j=right-1;j>=0;j--){
+                if(b[j]>b[j+1]){
+                    int tmp=b[j];
+                    b[j]=b[j+1];
+                    b[j+1]=tmp;
+                    shift=j;
+                }
+            }
+            left=shift;
+        }
+        for(int i:b){
             System.out.print(i+" ");
         }
     }
